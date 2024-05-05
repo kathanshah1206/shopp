66cd9799980b79f871d790e2e9eef9da15e551f3
kathan shah <kathanshah12062002@gmail.com>
2024-05-05 15:16:57 +0530

    7

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,8 +8,12 @@ const createProduct = async (req, res, next) => {
     product,
   });
 };
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: "working" });
+const getAllProducts = async (req, res) => {
+  const product = await Product.find();
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
 
 export { getAllProducts, createProduct };
