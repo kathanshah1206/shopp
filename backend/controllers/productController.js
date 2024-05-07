80cfe5a1b0d4bccdc3cb7aa01b4134cec8392cd0
kathan shah <kathanshah12062002@gmail.com>
2024-05-07 23:22:05 +0530

    9

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,22 +1,22 @@
 import Product from "../models/productModel.js";
 import ErrorHandler from "../utils/errorhandler.js";
-
+import catchAsyncError from "../middleware/catchAsyncError.js";
 //create a product
-const createProduct = async (req, res, next) => {
+const createProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(200).json({
     success: true,
     product,
   });
-};
+});
 //get all produccts
-const getAllProducts = async (req, res) => {
+const getAllProducts = catchAsyncError(async (req, res) => {
   const product = await Product.find();
   res.status(200).json({
     success: true,
     product,
   });
-};
+});
 
 //update prodict
 const updateProduct = async (req, res, next) => {
--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,25 @@ config({ path: "config/config.env" });
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
+
+process.on("uncaughtException", (err) => {
+  console.log(
+    chalk.underline.red(
+      `erorr ${err.message}..shutting down die to unhandled excepto=ion`
+    )
+  );
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(
+    chalk.underline.red(
+      `error: ${err.message}..shutting down die to unhandled promise rejevtijin`
+    )
+  );
+  server.close(() => {
+    process.exit(1);
+  });
+});
