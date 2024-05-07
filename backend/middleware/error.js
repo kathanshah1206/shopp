2090df144087bd60c9a103ca81355fe21ebd740e
kathan shah <kathanshah12062002@gmail.com>
2024-05-07 23:22:54 +0530

    10

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,9 +3,14 @@ import ErrorHandler from "../utils/errorhandler.js";
 const errorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server error";
+
+  if (err.name == "CastError") {
+    const msg = `Resourece not Found.Invalid ${err.path}`;
+    err = new ErrorHandler(msg, 400);
+  }
   res.status(err.statusCode).json({
     success: false,
-    error: err,
+    message: err.message,
   });
 };
 
